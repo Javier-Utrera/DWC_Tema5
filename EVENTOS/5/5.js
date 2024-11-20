a8fc4ac28aaeb54f0fff46969b6bf70616a2799e
Francisco Javier Rodriguez <francisco.rodriguez.garcia.al@iespoligonosur.org>
2024-11-20 14:35:50 +0100

    para casa

--- EVENTOS/5/5.js
+++ EVENTOS/5/5.js
@@ -1,7 +1,11 @@
 window.addEventListener("load",inicializar,false);
 function inicializar(){
-    document.getElementById("sms").addEventListener("click", contador, false);
+    document.getElementById("sms").addEventListener("keyup", contador, false);
 }
 function contador(e){
-    
+    const contadorx=145;
+
+    let caracteresRestantes = contadorx - e.currentTarget.value.length;
+    document.getElementById("caracteresRestantes").innerHTML="Caracteres restantes: " + caracteresRestantes;
+    console.log(e.currentTarget.validationMessage);
 }
